1f618af8e6f3c3462c2d19f2fe9880011da9fedb
Jason Cemra <cemrajc@gmail.com>
2016-05-23 19:52:30 +1000

    Working on a Dark Theme

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 #MungeGen
 
-Munge some random sounds together, make a silly word.
+Munge some random sounds together, make a silly word. Visit the [Github pages URL](http://cemrajc.github.io/mungegen) to use it, or run it yourself for offline/personal use.
 
-The sounds list needs to be revised, but so far, the interface works.
 
-### Why is this here...
+### How to run it yourself
+
+You have two options:
+
+1. Download the source code as a zip and run the index.html file with your favourite (or most hated) web-browser
+2. Get the source code and run a local (or public) server to serve them static. It should work without too much help.
+
+### Why does this even exist?!
 
 Well, I was bored once and decided that the way syllables meld together in a pronounceable way was actually interesting. This is the result of my curiosity - it's not perfect, but it works.
 
-Enjoy :P
+Enjoy :P<br>
+***~Jason Cemra***
--- main.js
+++ main.js
@@ -2,52 +2,28 @@ document.addEventListener("DOMContentLoaded", function(){
 
     var out = document.querySelector('header');
     var word_list = document.querySelector('aside');
-    var button = document.querySelector('button');
+    var generate = document.querySelector('[role=generate]');
+    var theme = document.querySelector('[role=theme]');
     var length_seed = document.querySelector('input');
 
     var generated_words = [];
-    var sound_list = [
-        "mung",
-        "bung",
-        "wagga",
-        "mug",
-        "ah",
-        "bing",
-        "moo",
-        "wul",
-        "ngah",
-        "jng",
-        "dirp",
-        "oo",
-        "gum",
-        "ham",
-        "cung",
-        "ber",
-        "bla",
-        "bng",
-        "gngij",
-        "wig",
-        "wng",
-        "ng",
-        "iwg",
-        "jwig",
-        "gmi",
-        "jnigi",
-        "ikl",
-        "imn",
-        "igj",
-        "yu",
-        "yim",
-        "yur",
-        "ah",
-        "yji",
-        "ylu",
-        "yert",
-        "ygim"
-    ];
 
 
-    button.addEventListener('click', function(){
+    theme.addEventListener('click', function(){
+        var msg = theme.innerHTML;
+        if (msg == "Dark Theme") {
+            setDarkTheme();
+            theme.innerHTML = 'Light Theme';
+            theme.style.background = "#E1E1E1";
+            theme.style.color = "#222";
+        } else {
+            theme.innerHTML = 'Dark Theme'
+            theme.style.background = "#1E1E1E";
+            theme.style.color = "#E1E1E1";
+        };
+    });
+
+    generate.addEventListener('click', function(){
         var syllable_quantity = Math.min(Math.max(Math.round(Math.random() * length_seed.value), 2), 10);
         var word = wordGen(syllable_quantity);
 
@@ -56,23 +32,32 @@ document.addEventListener("DOMContentLoaded", function(){
         generated_words.unshift("<p>" + word + "</p>");
 
         word_list.innerHTML = generated_words.join("\n");
-    })
+    });
 
 
-    function wordGen(len) {
-        var array_len = sound_list.length - 1;
-        var result = "";
-
-        for (var i = 0; i < len; i++) {
-            var rand = Math.round(Math.random() * array_len);
-            result += sound_list[rand];
-        };
-
-        return titleCase(result);
-    }
-
-    function titleCase(word) {
-        return word[0].toUpperCase() + word.slice(1);
+    function setDarkTheme(){
+        document.body.style.background = "#1a1a1a";
+        document.body.style.color = "#eee";
+        word_list.style.color = "#eee !important";
+        out.style.color = "#eee";
+        generate.style.boxShadow = "none";
+        theme.style.boxShadow = "none";
     }
 
 });
+
+function wordGen(len) {
+    var array_len = sound_list.length - 1;
+    var result = "";
+
+    for (var i = 0; i < len; i++) {
+        var rand = Math.round(Math.random() * array_len);
+        result += sound_list[rand];
+    };
+
+    return titleCase(result);
+}
+
+function titleCase(word) {
+    return word[0].toUpperCase() + word.slice(1);
+}
